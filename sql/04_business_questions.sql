1e916eb521baebee1bab565d92214c4a72915da2
pavan kumar bathula <pavankumarbathula@Mac.lan>
2026-07-15 19:40:10 -0400

    Implemented advanced SQL analysis with CTEs and window functions

--- sql/04_business_questions.sql
+++ sql/04_business_questions.sql
@@ -262,15 +262,19 @@ SELECT
 FROM orders
 WHERE order_delivered_customer_date IS NOT NULL;
 
--- Q22. Which states have the longest average delivery  time?
+-- Q22. Which states have the longest average delivery time?
 
-SELECT C.customer_state,ROUND(AVG(EXTRACT(EPOCH FROM
-(O.order_delivered_customer_date - O.order_purchase_timestamp)) / 86400 ),2)
-AS AVG_DELIVERY_DAYS
-FROM orders O JOIN customers C
-ON C.customer_id=O.customer_id
-WHERE o.order_delivered_customer_date IS NOT NULL
-GROUP BY C.customer_state
+WITH DELIVER_TIME AS (
+SELECT C.customer_state,
+ROUND(AVG(EXTRACT(EPOCH FROM(O.order_delivered_customer_date - O.order_purchase_timestamp))
+/86400),2) AS AVG_DELIVERY_DAYS
+FROM orders O
+JOIN customers C
+ON C.customer_id = O.customer_id
+WHERE O.order_delivered_customer_date IS NOT NULL
+GROUP BY C.customer_state)
+
+SELECT * FROM DELIVER_TIME
 ORDER BY AVG_DELIVERY_DAYS DESC;
 
 
@@ -299,6 +303,8 @@ SELECT ROUND((t.TOTAL_ORDERS_PLACED - L.NO_OF_ORDERS_DELIVERED_LATE)
 *100.0/t.TOTAL_ORDERS_PLACED,2)AS ON_TIME_DELIVERY_PERCENTAGE
 FROM TOTAL_ORDERS t CROSS JOIN LATE_DELIVERY L;
 
+
+
 -- ============================================================
 -- SECTION 6: CUSTOMER REVIEW ANALYSIS
 -- Purpose:
@@ -307,15 +313,68 @@ FROM TOTAL_ORDERS t CROSS JOIN LATE_DELIVERY L;
 
 -- Q25. What is the average review score?
 
+SELECT ROUND(AVG(review_score),2) AS AVG_REVIEW_SCORE
+FROM order_reviews;
+
 -- Q26. How are review scores distributed?
+SELECT review_score, COUNT(review_score) AS no_of_reviews
+FROM order_reviews
+GROUP BY review_score
+ORDER BY review_score;
 
 -- Q27. Which product categories have the highest average review score?
 
+WITH order_categories AS (
+    SELECT DISTINCT
+        oi.order_id,
+        ct.product_category_name_english AS category
+    FROM order_items oi
+    JOIN products p
+        ON oi.product_id = p.product_id
+    JOIN category_translation ct
+        ON p.product_category_name = ct.product_category_name)
+SELECT
+    oc.category,
+    ROUND(AVG(orr.review_score), 2) AS avg_review_score
+FROM order_categories oc
+JOIN order_reviews orr
+    ON oc.order_id = orr.order_id
+GROUP BY
+    oc.category
+ORDER BY
+    avg_review_score DESC;
+
 -- Q28. Which product categories have the lowest average review score?
+
+WITH order_categories AS (
+    SELECT DISTINCT
+        oi.order_id,
+        ct.product_category_name_english AS category
+    FROM order_items oi
+    JOIN products p
+        ON oi.product_id = p.product_id
+    JOIN category_translation ct
+        ON p.product_category_name = ct.product_category_name)
+SELECT
+    oc.category,
+    ROUND(AVG(orr.review_score), 2) AS avg_review_score
+FROM order_categories oc
+JOIN order_reviews orr
+    ON oc.order_id = orr.order_id
+GROUP BY
+    oc.category
+ORDER BY
+    avg_review_score ASC;
 
 -- Q29. Does delivery time affect review scores?
 
-
+SELECT ROUND(AVG(EXTRACT(EPOCH FROM(O.order_delivered_customer_date - O.order_purchase_timestamp))/86400),2)
+AS AVG_DELIVERY_DAYS, ORR.review_score AS REVIEW_SCORE
+FROM Orders O JOIN order_reviews ORR
+ON O.order_id = ORR.order_id
+WHERE O.order_delivered_customer_date IS NOT NULL
+GROUP BY REVIEW_SCORE
+ORDER BY REVIEW_SCORE ASC;
 
 -- ============================================================
 -- SECTION 7: PAYMENT ANALYSIS
@@ -325,13 +384,30 @@ FROM TOTAL_ORDERS t CROSS JOIN LATE_DELIVERY L;
 
 -- Q30. Which payment methods are most commonly used?
 
+SELECT payment_type, COUNT(*) AS COUNTS
+FROM order_payments
+GROUP BY payment_type
+ORDER BY COUNTS DESC;
+
 -- Q31. Which payment methods generate the highest revenue?
+
+SELECT payment_type, SUM(payment_value) AS highest_revenue
+FROM order_payments 
+GROUP BY payment_type
+ORDER BY highest_revenue DESC;
 
 -- Q32. What is the average payment installment count?
 
+SELECT ROUND(AVG(payment_installments),2)
+FROM order_payments
+WHERE payment_installments != 0;
+
 -- Q33. Which payment method has the highest average transaction value?
 
-
+SELECT payment_type, ROUND(AVG(payment_value),2) AS highest_revenue
+FROM order_payments 
+GROUP BY payment_type
+ORDER BY highest_revenue DESC;
 
 -- ============================================================
 -- SECTION 8: TIME-BASED SALES ANALYSIS
@@ -341,15 +417,69 @@ FROM TOTAL_ORDERS t CROSS JOIN LATE_DELIVERY L;
 
 -- Q34. What are the monthly sales trends?
 
+SELECT DATE_TRUNC('month', O.order_purchase_timestamp)::DATE AS ORDERED_MONTH, 
+	SUM(OP.payment_value) AS total_sales
+FROM orders O JOIN order_payments OP
+ON O.order_id = OP.order_id
+GROUP BY ORDERED_MONTH
+ORDER BY ORDERED_MONTH;
+
 -- Q35. What are the monthly order trends?
+
+SELECT DATE_TRUNC('month', order_purchase_timestamp)::DATE AS ORDERED_MONTH, 
+	COUNT(order_id) AS total_orders
+FROM orders
+GROUP BY ORDERED_MONTH
+ORDER BY ORDERED_MONTH;
 
 -- Q36. Which month generated the highest revenue?
 
+SELECT DATE_TRUNC('month', O.order_purchase_timestamp)::DATE AS ORDERED_MONTH, 
+	SUM(OP.payment_value) AS total_sales
+FROM orders O JOIN order_payments OP
+ON O.order_id = OP.order_id
+GROUP BY ORDERED_MONTH
+ORDER BY total_sales DESC 
+LIMIT 1;
+
 -- Q37. Which month had the highest number of orders?
+
+SELECT DATE_TRUNC('month', order_purchase_timestamp)::DATE AS ORDERED_MONTH, 
+	COUNT(order_id) AS total_orders
+FROM orders
+GROUP BY ORDERED_MONTH
+ORDER BY total_orders DESC
+LIMIT 1;
+
 
 -- Q38. What is the month-over-month revenue growth?
 
+-- Q38. What is the month-over-month revenue growth?
 
+WITH monthly_sales AS (
+    SELECT DATE_TRUNC('month', O.order_purchase_timestamp)::DATE AS ordered_month,
+    SUM(OP.payment_value) AS monthly_revenue
+    FROM orders O
+    JOIN order_payments OP
+    ON O.order_id = OP.order_id
+    GROUP BY ordered_month
+),
+
+previous_month_sales AS (
+    SELECT ordered_month, monthly_revenue,
+        LAG(monthly_revenue) OVER (ORDER BY ordered_month) AS previous_month_revenue
+    FROM monthly_sales
+)
+
+SELECT
+    ordered_month,
+    ROUND(monthly_revenue, 2) AS monthly_revenue,
+    ROUND(previous_month_revenue, 2) AS previous_month_revenue,
+    ROUND((monthly_revenue - previous_month_revenue)
+        * 100.0 / previous_month_revenue,2) 
+		AS mom_growth_percentage
+FROM previous_month_sales
+ORDER BY ordered_month;
 
 -- ============================================================
 -- SECTION 9: ADVANCED BUSINESS ANALYSIS
@@ -359,9 +489,41 @@ FROM TOTAL_ORDERS t CROSS JOIN LATE_DELIVERY L;
 
 -- Q39. Which orders generated the highest revenue?
 
+SELECT O.order_id, SUM(OP.payment_value) AS revenue
+FROM orders O JOIN order_payments OP
+ON O.order_id = OP.order_id
+GROUP BY O.order_id
+ORDER BY revenue DESC
+LIMIT 10;
+
 -- Q40. Which sellers sell across the most customer states?
 
+SELECT S.seller_id, COUNT(DISTINCT C.customer_state) AS NO_OF_STATES
+FROM sellers S JOIN order_items OI
+ON S.seller_id = OI.seller_id
+JOIN orders O
+ON O.order_id = OI.order_id
+JOIN customers C 
+ON C.customer_id = O.customer_id
+GROUP BY S.seller_id
+ORDER BY NO_OF_STATES DESC;
+
 -- Q41. Which customer states purchase the widest variety of product categories?
+
+SELECT C.customer_state,COUNT(DISTINCT(CT.product_category_name_english)) AS NO_OF_CATEGORIES,
+STRING_AGG(DISTINCT CT.product_category_name_english,', '
+ORDER BY CT.product_category_name_english) AS categories
+FROM category_translation CT JOIN products P
+ON CT.product_category_name = P.product_category_name
+JOIN order_items OI
+ON OI.product_id = P.product_id
+JOIN orders O
+ON O.order_id = OI.order_id
+JOIN customers C
+ON C.customer_id = O.customer_id
+GROUP BY C.customer_state
+ORDER BY NO_OF_CATEGORIES DESC;
+
 
 -- Q42. What percentage of total revenue comes from the top 10 product categories?
 
